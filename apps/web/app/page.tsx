6fcbeaaefe5e4c3a582e93b92ebe8566ace89abd
Théo Gillet <90395803+theo-code33@users.noreply.github.com>
2023-08-02 10:52:06 +0200

    show school name

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,9 +1,11 @@
 import { Button, Header } from "ui";
 
 export default function Page() {
+  const school = process.env.NEXT_PUBLIC_SCHOOL;
   return (
     <>
       <Header text="Web" />
+      <h1>{school}</h1>
       <Button />
     </>
   );
